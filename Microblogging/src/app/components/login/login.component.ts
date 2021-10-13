146579ca0a84e77c8adf9c0a2f732871d1673eb9
cptvictor <victorsyanwenliu19@gmail.com>
2021-10-13 17:40:18 -0400

    finished login component

--- Microblogging/src/app/components/login/login.component.ts
+++ Microblogging/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthorizationService } from 'src/app/services/authorization.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -7,21 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router, private userService: UserService, private authService: AuthorizationService) { }
 
-  ngOnInit(): void 
-  {
+  ngOnInit(): void {
   }
 
   username: string = "";
   password: string = "";
   failedLogin: boolean = false;
 
-  login()
-  {
-  }
-
-  register()
-  {
+  /**
+   * Perform a user login. A successful login will set the JWT for the session and pull a list of 
+   * TechnologyCategories, followed by routing the user to their dashboard. Incorrect login credentials
+   * will display a message informing the user of their error.
+   * 
+   * Current limitation: application will wait for TechnologyCategories table to be retrieved.
+   * Login will not finish if the request never completes.
+   */
+  login() {
+    this.userService.login(this.username, this.password).subscribe((result) => 
+    {
+      this.failedLogin = false;
+      this.authService.setJwt(result);
+      console.log("YAY");
+    },
+    (error) => 
+    {
+      console.log(error);
+      this.failedLogin = true;
+    })
   }
 }
