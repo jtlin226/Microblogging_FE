e34fb0283d926d3347b6fa182c9fcde076661cc4
Jintao Lin <jtlin226@gmail.com>
2021-10-17 22:48:21 -0500

    added css

--- Microblogging/src/app/components/main-page/main-page.component.ts
+++ Microblogging/src/app/components/main-page/main-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Micro } from 'src/app/models/micro';
+import { User } from 'src/app/models/user';
 import { MicroService } from 'src/app/services/micro.service';
 import { UserService } from 'src/app/services/user.service';
 
@@ -12,24 +13,30 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class MainPageComponent implements OnInit {
 
-  content: string = "";
+  content: string;
+  username: string;
+  micros: Micro[] = [];
+  about: string;
+  user: User;
+
   constructor(private router : Router, private modalService: NgbModal, private userService: UserService, private microService: MicroService) { }
 
-  micros: Micro[] = [];
+  
   
   ngOnInit() {
     this.microService.getMicros().subscribe(result => this.micros = result);
-    console.log(this.micros);
+    this.username = this.user.username;
+    this.about = this.user.about;
+    console.log(this.username);
   }
 
   open(content: any) {
     this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'});
   }
   createMicro() {
-    let newMicro = {
-      id: 0,
-      content: this.content,
-      user: this.userService.getUserId()
-    }
+    console.log(this.content);
+    console.log("Creating...")
+    this.microService.createMicro(this.content);
+    console.log("Created...")
   }
 }
--- Microblogging/src/app/services/micro.service.ts
+++ Microblogging/src/app/services/micro.service.ts
@@ -1,15 +1,17 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { convertToObject } from 'typescript';
 import { Micro } from '../models/micro';
 import { AuthorizationService } from './authorization.service';
+import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MicroService {
 
-  constructor(private http: HttpClient, private authService: AuthorizationService) { }
+  constructor(private http: HttpClient, private authService: AuthorizationService, private userService: UserService) { }
 
   url = "http://localhost:8082/micro";
 
@@ -20,6 +22,15 @@ export class MicroService {
     })
   };
 
+  convertToDto(content: string) {
+    let dto = {
+      id: 0,
+      content: content,
+      user: this.userService.getUserObject()
+    }
+    return dto;
+  }
+
   getMicros(): Observable<Micro[]>{
     this.httpOptions.headers = this.httpOptions.headers.set('Authorization', `Bearer ${this.authService.jwt}`);
     console.log(this.authService.jwt)
@@ -27,4 +38,11 @@ export class MicroService {
     return this.http.get<Micro[]>(this.url, this.httpOptions)
   }
 
+  createMicro(newContent: string) {
+    this.httpOptions.headers = this.httpOptions.headers.set('Authorization', `Bearer ${this.authService.jwt}`);
+    console.log("Converting to DTO...");
+    const dto = this.convertToDto(newContent);
+    console.log(dto);
+    return this.http.post<Micro>(this.url, dto, this.httpOptions)
+  }
 }
